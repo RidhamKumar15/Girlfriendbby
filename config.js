d66bfab3ebe869818f10e27aeb4ffc57275fc200
RIDHAM KUMAR <150554802+RidhamKumar15@users.noreply.github.com>
2025-05-02 02:34:09 +0530

    added new project

---
+++ config.js
@@ -0,0 +1,22 @@
+const CONFIG = {
+    titleWeb: "Website Title",
+    introTitle: 'Rakhiba ji',
+    introDesc: `The earth is so strange
+    Yet somehow you keep taking the wrong path
+    Wandering into my lonely heart
+    Behind the word love is affection`,
+    btnIntro: '^^HiHi^^',
+    title: 'Did you fall in love at first sight? 🥰',
+    desc: 'Did you fall for me the moment you saw that smile?',
+    btnYes: 'I like you a lot <33',
+    btnNo: 'Nope :3',
+    question: 'Out of 7 billion people in the world, why do you love me? <3',
+    btnReply: 'Send to you <3',
+    reply: 'Love means love, even if you don’t, I still love <33333333',
+    mess: 'I knew it 🥰. Love you lots 😘😘',
+    messDesc: 'This weekend lets meet at your place !!',
+    btnAccept: 'Yesss okayyy <3',
+    // messLink: 'https://github.com/zukahai/Confess-Crush' // your message link or any redirection
+    messLink: 'shayari.html' // cute shayari for Rakhiba
+
+}
